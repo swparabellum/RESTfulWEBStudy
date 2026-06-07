4504178fbde21259bcea0f99117d54bd80af0a5a
Kang.SW <swparabellum@gmail.com>
2026-06-07 16:27:12 +0900

    누락된 import 추가

--- board-backend/src/main/java/com/example/board_backend/security/SecurityConfig.java
+++ board-backend/src/main/java/com/example/board_backend/security/SecurityConfig.java
@@ -1,5 +1,6 @@
 package com.example.board_backend.security;
 
+import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
